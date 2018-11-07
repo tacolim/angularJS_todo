158130b65265b8292089c4121cca52acdb9bd55d
Cassidy SN Avery <tacolim@gmail.com>
2018-11-06 23:47:53 -0500

    able to add and remove tasks; some CSS

--- todoController.js
+++ todoController.js
@@ -1,14 +1,19 @@
 app.controller("todoController", ($scope) => {
-    $scope.tasks = [];
+    $scope.saved = localStorage.getItem('tasks');
+    $scope.tasks = (localStorage.getItem('tasks')!==null) ? JSON.parse($scope.saved) : [ {text: 'Set some tasks & DO \'EM!', done: false} ];
+    
+    localStorage.setItem('tasks', JSON.stringify($scope.tasks));
 
     $scope.add = function() {
-        $scope.tasks.push({text:$scope.task, done:false});
+        $scope.tasks.push({text: $scope.task, done: false});
         $scope.task = '';
+        localStorage.setItem('tasks', JSON.stringify($scope.tasks));
     };
 
     $scope.remove = function () {
         $scope.tasks = $scope.tasks.filter(task => {
             return !task.done;
         });
+        localStorage.setItem('tasks', JSON.stringify($scope.tasks));
     };
 });
